69bb5ca5df0f3f4253064339339902b48d74feed
manuxx <manus@eti.pg.gda.pl>
2016-07-06 19:03:15 +0200

    sample spec for testing purpose

---
+++ SampleSpec/SampleSpec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Machine.Specifications;
+
+namespace SampleSpec
+{
+    [Subject(typeof (int))]
+    public class when_increasing_int 
+    {
+        Establish c = () => {  sut = 1; };
+
+        Because b = () => { sut = sut + 1; };
+
+        It should_be_equal_to_two = () => { sut.ShouldEqual(2); };
+
+        private static int sut;
+    }
+}
